e7bd9a20e6f5c3929d29022675444ed34c08b8ea
merriman23 <77376248+merriman23@users.noreply.github.com>
2021-03-30 12:23:37 -0600

    Problems 13-18 on different computer

--- array.py
+++ array.py
@@ -11,3 +11,8 @@ cars.pop(4)
 print(cars)
 cars.sort()
 print(cars)
+cars.sort(reverse = True)
+print(cars)
+my_array_length = len(cars)
+array_string = 'The length of my array is '
+print(array_string,my_array_length)
